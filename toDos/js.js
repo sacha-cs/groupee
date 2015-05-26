92b99a8bc790baac1afb7f780ce166863ef66c65
rgu13 <rgu13@imperial.ac.uk>
2015-05-26 17:33:59 +0100

    User interface for To do lists

---
+++ toDos/js.js
@@ -0,0 +1,19 @@
+function addToDo() { 
+}
+
+function updateToDo() { 
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+	}
+
+	function drag(ev) {
+   		ev.dataTransfer.setData("text", ev.target.id);
+	}
+
+	function drop(ev) {
+    	ev.preventDefault();
+    	var data = ev.dataTransfer.getData("text");
+    	ev.target.appendChild(document.getElementById(data));
+	}
