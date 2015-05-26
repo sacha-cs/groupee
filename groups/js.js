775eb4669edc733dd46b8da20768ee91e99359d0
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2015-05-26 09:12:11 +0100

    Fix bug of empty fields on group creation page not allowing user to ever create group

--- groups/js.js
+++ groups/js.js
@@ -1,15 +1,16 @@
-var waitingForResponse = false;
-
 /* To be called when the 'Create!' button is clicked in the Create page. */
 function createGroup() {
-    if (waitingForResponse) {
-        return;
-    }
-    waitingForResponse = true;
     var groupname = document.getElementById("group_name").value;
     var description = document.getElementById("description").value;
     var e = document.getElementsByClassName("privacy-dropdown")[0];
     var privacy = e.options[e.selectedIndex].text;
+
+    setErrorText("");
+    if(groupname == "" || description == "")
+    {
+        setErrorText("Please fill out all fields.");
+        return;
+    }
 
     var aClient = new HttpClient();
     aClient.post('create', 'group_name=' + groupname + 
@@ -18,14 +19,16 @@ function createGroup() {
         function (response) { 
             var correct = response[0];
             if(correct == "N") {
-                waitingForResponse = false;
                 switch(response.slice(1)) {
                     case "NUserExistsInGroup":
                         setErrorText("You are already a member of this group.");
                         break;
+                    case "EmptyFields":
+                        setErrorText("Please fill out all the fields.");
+                        break;
                 }
             } else { // No problems.
-               window.location = "add_users.html"; 
+                window.location = "add_users.html"; 
             } 
         }
     );
