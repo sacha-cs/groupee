f1c90fda2faac78eec155449168fe93907f261cc
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2015-05-27 19:21:28 +0100

    Redirect user to login page when Log Out is clicked on

--- home/js.js
+++ home/js.js
@@ -1,3 +1,4 @@
+/* Loads the navigation bar with information about the current user and its group. */
 function loaded() {
     var groupName = getCookie("group-name");
     var userName = getCookie("username");
@@ -5,4 +6,8 @@ function loaded() {
     var userText = document.getElementById("navbar-self");
     groupText.innerHTML = "<p>" + groupName + "</p>";
     userText.innerHTML = "<p>Welcome, " + userName + "!</p>";
+}
+
+function logout() {
+    /* TODO: Clear session cookie. */
 }
