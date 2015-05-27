0cf64fc92dfdf3f9d571fd77cfcef3135abde3fb
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2015-05-27 19:09:53 +0100

    Add user's name to navbar

--- home/js.js
+++ home/js.js
@@ -1,5 +1,8 @@
 function loaded() {
     var groupName = getCookie("group-name");
+    var userName = getCookie("username");
     var groupText = document.getElementById("navbar-group");
+    var userText = document.getElementById("navbar-self");
     groupText.innerHTML = "<p>" + groupName + "</p>";
+    userText.innerHTML = "<p>Welcome, " + userName + "!</p>";
 }
