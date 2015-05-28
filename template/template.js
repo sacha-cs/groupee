c5fd9de85f013af51a8a042c9d81a709e3b633e5
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-05-28 17:46:43 +0100

    Glorify the chat and fresh scrollbar

--- template/template.js
+++ template/template.js
@@ -13,7 +13,14 @@ function toggleChat() {
 	}
 
 }
-function loaded() {
+function templateLoaded() {
+    var groupName = getCookie("group-name");
+    var userName = getCookie("username");
+    var groupText = document.getElementById("navbar-group");
+    var userText = document.getElementById("navbar-self");
+    groupText.innerHTML = "<p>" + groupName + "</p>";
+    userText.innerHTML = "<p>Welcome, " + userName + "!</p>";
+
 	document.getElementById("chat").addEventListener("mousewheel", function (e) {
 	    var delta = e.wheelDelta || -e.detail;
 	    
@@ -27,4 +34,14 @@ function loaded() {
 	    e.preventDefault();
 	});
 
+	loaded();
+}
+
+/* Log out the current user, and clear the session cookie. */
+function logout() { 
+    document.cookie="seshCookie=;path=/";
+}
+
+function goTo(page) {
+	window.location = "/" + page;
 }
