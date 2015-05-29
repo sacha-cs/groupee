5356c7d26b0e4bf472b1a9d2acd09f6bd6e7bd40
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-05-29 22:51:17 +0100

    Chat toggle button styled

--- template/template.js
+++ template/template.js
@@ -5,10 +5,10 @@ function toggleChat() {
 	chatOpen = !chatOpen;
 
 	if (chatOpen) {
-		document.getElementById("content").style.left = "300px";
+		document.getElementById("content").style.left = "350px";
 		document.getElementById("chat-toggle").style.left = "285px";
 	} else {
-		document.getElementById("content").style.left = "15px";
+		document.getElementById("content").style.left = "50px";
 		document.getElementById("chat-toggle").style.left = "0px";		
 	}
 
