fa4c92e43387958b676f3e348d357f6d8880c510
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-05-30 23:41:17 +0100

    More styling in the navbar (now with avatars) and changed size of whiteboard toolbox

--- home/js.js
+++ home/js.js
@@ -2,10 +2,12 @@
 function loaded() {
     var groupName = getCookie("group-name");
     var userName = getCookie("username");
+    var avatarImage = document.getElementById("avatar-self");
     var groupText = document.getElementById("navbar-group");
     var userText = document.getElementById("navbar-self");
     groupText.innerHTML = "<p>" + groupName + "</p>";
     userText.innerHTML = "<p>Welcome, " + userName + "!</p>";
+    avatarImage.innerHTML = "<img src='http://www.doc.ic.ac.uk/project/2014/271/g1427136/avatars/" + userName + ".png'>"
 }
 
 /* Log out the current user, and clear the session cookie. */
--- template/template.js
+++ template/template.js
@@ -18,8 +18,10 @@ function templateLoaded() {
     var userName = getCookie("username");
     var groupText = document.getElementById("navbar-group");
     var userText = document.getElementById("navbar-self");
+    var avatarImage = document.getElementById("avatar-self");
     groupText.innerHTML = "<p>" + groupName + "</p>";
     userText.innerHTML = "<p>Welcome, " + userName + "!</p>";
+    avatarImage.innerHTML = "<img src='http://www.doc.ic.ac.uk/project/2014/271/g1427136/avatars/" + userName + ".png'>"
 
 	document.getElementById("chat").addEventListener("mousewheel", function (e) {
 	    var delta = e.wheelDelta || -e.detail;
