256913ec7d3faaf1b10b39604956688986ec553d
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-05-31 18:45:33 +0100

    Add navbar to not in groups pages with avatar...

---
+++ template/no_group_template.js
@@ -0,0 +1,15 @@
+function templateLoaded() {
+    var userName = getCookie("username");
+    var userText = document.getElementById("navbar-self");
+    var avatarImage = document.getElementById("avatar-self");
+    userText.innerHTML = "<p>Welcome, " + userName + "!</p>";
+    avatarImage.innerHTML = "<img src='http://www.doc.ic.ac.uk/project/2014/271/g1427136/avatars/" + userName + ".png'>"
+
+	loaded();
+}
+
+/* Log out the current user, and clear the session cookie. */
+function logout() { 
+    document.cookie="seshCookie=;path=/";
+}
+
