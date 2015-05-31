fdd0ac4540202e717f914e7b716f910fe2dd1f39
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2015-05-31 10:17:36 +0100

    Change structure of local note storage

--- posts/js.js
+++ posts/js.js
@@ -1,11 +1,12 @@
 var noteInfo = [];
-var lastId = 0;
 var imgStr = 'http://www.doc.ic.ac.uk/project/2014/271/g1427136/icons/delete.png';
 
 // Adds a new note.
 function addNote() {
     var notes = document.getElementById("notes"); 
     var noteLength = notes.children.length;
+
+    lastId = ((noteLength > 0) ? parseInt(notes.children[noteLength-1].id.slice(4))+1 : 0);
 
     // Create an empty note.
     notes.innerHTML += "<li id='note" + lastId + "'>" + 
@@ -20,42 +21,61 @@ function addNote() {
     document.getElementById("title" + lastId).addEventListener("blur", sendUpdate);
     document.getElementById("content" + lastId).addEventListener("blur", sendUpdate);
         
-    noteInfo[lastId] = {noteId: -1, title: "", content: "", saved: false};
-    lastId++;
+    noteInfo[lastId] = {listId: lastId, noteId: -1, title: "", content: "", saved: false};
+    //lastId++;
 
     // Iterate through all notes, set their values and re-add the 'lost' event listeners. 
     for (var i = 0; i < noteLength; i++) {
         var currentId = parseInt(notes.children[i].id.slice(4));
-        document.getElementById("title" + currentId).value = noteInfo[currentId].title;
-        document.getElementById("content" + currentId).value = noteInfo[currentId].content;
-        
+        console.log("current id; " + currentId);
+
+        // Find corresponding element in array of notes.
+        var currentNote = noteInfo.filter(function(o) {
+            return o.listId === currentId;
+        })[0];
+        var currentTitle = currentNote.title;
+        var currentContent = currentNote.content;
+
+        document.getElementById("title" + currentId).value = currentTitle;
+        document.getElementById("content" + currentId).value = currentContent;
+      
         // Strings are immutable in Javascript, so the whole HTML string is written over! 
         document.getElementById("title" + currentId).addEventListener("blur", sendUpdate);
         document.getElementById("content" + currentId).addEventListener("blur", sendUpdate);
     }
+    console.log(noteInfo);
 }
 
 // Handles the saving of a note to the DB / altering an existing note.
 function sendUpdate(e) {
+    console.log("gonna send update");
     var id = e.target.parentElement.parentElement.id.slice(4); // Id of note we just clicked away from.
+    console.log("for id " + id);
+    // Find corresponding element in array of notes.
+    var currentNote = noteInfo.filter(function(o) {
+        return o.listId === id;
+    })[0];
+    console.log("noteInfo:" );
+    console.log(noteInfo);
 
-    var expectedTitle = noteInfo[id].title;
-    var expectedContent = noteInfo[id].content; 
+    var expectedTitle = currentNote.title;
+    var expectedContent = currentNote.content; 
 
     var actualTitle = document.getElementById("title" + id).value;
     var actualContent = document.getElementById("content" + id).value;
 
-    var data = {noteTitle: actualTitle, noteContent: actualContent, noteId: 2129};
+    var data = {noteTitle: actualTitle, noteContent: actualContent, noteId: -1};
 
     // We don't want to do anything if there are no changed that have been made to the note. 
     if (expectedTitle == actualTitle && expectedContent == actualContent) {
         return;
     }
+
     // Something is different, so we can send some data. 
     var aClient = new HttpClient();
-    if (noteInfo[id].saved) {
+    if (currentNote.saved) {
         // Update the note with the given id.
-        data.noteId = noteInfo[id].noteId;
+        data.noteId = currentNote.noteId;
         aClient.post('update_note', JSON.stringify(data),
             function(response) {
                 var correct = response[0];
@@ -71,8 +91,8 @@ function sendUpdate(e) {
                 var correct = response[0];
                 if (correct == "Y") {
                     var newId = response.slice(1); // Id of note that we have saved to.
-                    noteInfo[id].saved = true;
-                    noteInfo[id].noteId = newId;
+                    currentNote.saved = true;
+                    currentNote.noteId = newId;
                 } else {
                     // TODO: Handle errors. 
                 }
@@ -81,8 +101,8 @@ function sendUpdate(e) {
     }
 
     // Record updates locally. 
-    noteInfo[id].title = actualTitle;
-    noteInfo[id].content = actualContent;
+    currentNote.title = actualTitle;
+    currentNote.content = actualContent;
 }
 
 function setErrorText(error) {
@@ -96,13 +116,20 @@ function getNotes() {
 
 // Delete a note associated with a user from the database.
 function deleteNote(id) {
-    console.log("gonna delete note with id: " + id);
-    console.log(noteInfo);
     var aClient = new HttpClient();
-    console.log("which is" + noteInfo[id]);
-    var actualId = noteInfo[id].noteId;
+    
+    // Find corresponding element in array of notes, get its DB id.
+    var actualId = noteInfo.filter(function(o) {
+        return o.listId === id;
+    })[0].noteId;
+
     var data = {noteId: actualId};
-    noteInfo.splice(id, 1);
+
+    // Remove corresponding element from array of notes.
+    noteInfo = noteInfo.filter(function(o){
+        return o.listId !== id;
+    })
+
     var noteForDeletion = document.getElementById("note" + id);
     noteForDeletion.parentElement.removeChild(noteForDeletion);
     
@@ -129,15 +156,16 @@ function getAllNotes() {
             var noteId = noteStuff[0].split("=")[1];
             var noteTitle = noteStuff[1].split("=")[1];
             var noteContent = noteStuff[2].split("=")[1];
-            var info = {noteId : noteId,
-            	        noteTitle : escapeHtml(decodeURIComponent(noteTitle)),   
-                        noteContent : escapeHtml(decodeURIComponent(noteContent)),
+            var info = {listId : i,
+                        noteId : noteId,
+            	        title : escapeHtml(decodeURIComponent(noteTitle)),   
+                        content : escapeHtml(decodeURIComponent(noteContent)),
                         saved: true};
             noteInfo.push(info); 
             notes.innerHTML += "<li id='note" + i + "'>" + 
                                   "<a href='#'>" +
-                                      "<textarea class='note-title' id='title" + i + "' placeholder='Untitled'>" + noteTitle + "</textarea>" +
-                                      "<textarea class='note-content' id='content" + i + "' placeholder='Your content here'>" + noteContent + "</textarea>" +
+                                      "<textarea class='note-title' id='title" + i + "' placeholder='Untitled'>" + info.title + "</textarea>" +
+                                      "<textarea class='note-content' id='content" + i + "' placeholder='Your content here'>" + info.content + "</textarea>" +
                                       "<img onclick='deleteNote(" + i + ")' id='delete' src='" + imgStr + "'>" +
                                   "</a> " +
                               "</li>";
