cc18ca86245cec276ee2cb1f754bc83c6631e56b
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-05-31 23:50:45 +0100

    Start styling user settings page + changing avatar is now on this page

---
+++ usersettings/js.js
@@ -0,0 +1,10 @@
+function loaded() {
+	var username = getCookie('username');
+	document.getElementById('avatar').innerHTML = "<img src='http://www.doc.ic.ac.uk/project/2014/271/g1427136/avatars/" 
+	+ username + ".png'>"
+	document.getElementById('username').value = username;
+}
+
+// function toggleAccountSettings() {
+// 	document.getElementById('account').style.display = 'block';
+// }
