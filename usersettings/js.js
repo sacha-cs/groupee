d22874abb087986317ae17b88081ddefd7459bff
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-06-03 14:34:41 +0100

    Remove groups settings

--- usersettings/js.js
+++ usersettings/js.js
@@ -1,27 +1,18 @@
 function loaded() {
 	var username = getCookie('username');
 	document.getElementById('avatar').innerHTML = "<img src='http://www.doc.ic.ac.uk/project/2014/271/g1427136/avatars/" 
-	+ username + ".png'>"
+	+ username + ".png'>";
 }
 
 function toggleAccountSettings() {
 	document.getElementById('account').style.display = 'block';
 	document.getElementById('password').style.display = 'none';
-	document.getElementById('groups').style.display = 'none';
 }
 
 function togglePasswordSettings() {
 	document.getElementById('password').style.display = 'block';
 	document.getElementById('account').style.display = 'none';
-	document.getElementById('groups').style.display = 'none';
 }
-
-function toggleGroupsSettings() {
-	document.getElementById('groups').style.display = 'block';
-	document.getElementById('account').style.display = 'none';
-	document.getElementById('password').style.display = 'none';
-}
-
 
 function changePassword() {
 
